ecb6de40f785c303019bb102641158678270df6f
Kevin Most <kevin@kevinmost.com>
2014-08-21 17:05:21 -0400

    Initial commit

---
+++ src/main/java/synthesio/Synthesio.java
@@ -0,0 +1,16 @@
+package synthesio;
+
+/**
+ * @author kevin
+ * @date 8/21/14
+ */
+public class Synthesio {
+
+    String key;
+
+    protected Synthesio(String apiKey) {
+        this.key = apiKey;
+    }
+
+
+}
